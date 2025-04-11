e53280425d891de84d544175f16fa438d2a8118d
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-04-11 15:22:57 +0200

    Feat: 1,2 02 strings complete

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,19 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = "Giovanni";
+console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = "22";
+console.log(number)
+const text = "Buongiorno mi chiamo Giovanni";
+console.log(text)
+const bool = "1"
+console.log(bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
